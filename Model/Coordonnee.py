424c245df44e9edbd1e15bca3e2e6ff3b2a7db58
morl0027 <steven.morlet@etudiant.univ-reims.fr>
2023-01-12 14:31:58 +0100

    Coordonnées implémentées : tests ok

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -19,3 +19,43 @@ def type_coordonnee(coord: tuple) -> bool:
         and coord[0] >= 0 and coord[1] >= 0
 
 
+def construireCoordonnee(li: int, co: int) -> tuple:
+    """
+    Cette fonction inscrit deux parametres 'li' et 'co' dans un tuple de coordonnees que l'on retourne.
+
+    :param li: numero de ligne d'une coordonnee.
+    :param co: numero de colonne d'une coordonnee.
+    :return: (li, co).
+    """
+    if not isinstance(li, int) or not isinstance(co, int):
+        raise TypeError(f"construireCoordonnee : Le numéro de ligne {type(li)} ou le numéro de colonne {type(co)} "
+                        f"ne sont pas des entiers")
+    if li < 0 or co < 0:
+        raise ValueError(f"construireCoordonnee : Le numéro de ligne ({li}) ou de colonne ({co}) ne sont pas positifs")
+    return (li, co)
+
+
+def getLigneCoordonnee(coord: tuple) -> int:
+    """
+    Cette fonction retourne le numero de ligne contenu dans la coordonnee passee en parametre.
+
+    :param coord: Tuple contenant les coordonnees (y, x)
+    :return: Valeur de ligne de la coordonnee (coord(y))
+    """
+    if not isinstance(coord, tuple) or (not isinstance(coord[0], int) or not isinstance(coord[1], int)) or \
+            (coord[0] < 0 or coord[1] < 0):
+        raise TypeError("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+    return coord[0]
+
+
+def getColonneCoordonnee(coord: tuple) -> int:
+    """
+    Cette fonction retourne le numero de colonne contenu dans la coordonnee passee en parametre.
+
+    :param coord: Tuple contenant les coordonnees (y, x)
+    :return: Valeur de ligne de la coordonnee (coord(x))
+    """
+    if not isinstance(coord, tuple) or (not isinstance(coord[0], int) or not isinstance(coord[1], int)) or \
+            (coord[0] < 0 or coord[1] < 0):
+        raise TypeError("getColonneCoordonnee : Le paramètre n’est pas une coordonnée")
+    return coord[1]
